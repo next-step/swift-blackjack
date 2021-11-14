1e2f848119c92660455d3da82ff8434571185e7d
Mephrine <dthwrld@gmail.com>
2021-11-14 16:41:22 +0900

    feat: Make a Player
    Own name and deck

---
+++ Blackjack/Player.swift
@@ -0,0 +1,13 @@
+//
+//  Player.swift
+//  Blackjack
+//
+//  Created by Mephrine on 2021/11/14.
+//
+
+import Foundation
+
+struct Player {
+	let name: String
+	private(set) var deck: [BlackjackCard]
+}
