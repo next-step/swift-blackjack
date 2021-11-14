177947d4f09505e4747f9b8bf1b554243e84b585
Mephrine <dthwrld@gmail.com>
2021-11-14 15:28:35 +0900

    test: Remove the drawn card when the dealer deals

--- BlackjackTest/BlackjackTest.swift
+++ BlackjackTest/BlackjackTest.swift
@@ -8,9 +8,15 @@
 import XCTest
 
 class BlackjackTest: XCTestCase {
-    func test_shouldGet2CardsWhenTheDealerDealsCards() throws {
-			let dealear = Dealer()
-			let blackjackCards = dealear.deal()
-			XCTAssertEqual(blackjackCards.count, 2)
-    }
+	func test_shouldGet2CardsWhenTheDealerDealsCards() throws {
+		let dealear = Dealer()
+		let blackjackCards = dealear.deal()
+		XCTAssertEqual(blackjackCards.count, 2)
+	}
+	
+	func test_shouldRemoveTheDrawnCardsWhenTheDealerDeals() throws {
+		let dealer = Dealer()
+		let blackjackCards = dealer.deal()
+		XCTAssertTrue(dealer.deck.contains(blackjackCards))
+	}
 }
