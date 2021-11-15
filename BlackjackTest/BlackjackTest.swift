fbcac06730bef61756b5c21fc877074cb023576a
BeomKey <nkb5813@naver.com>
2021-11-15 21:42:40 +0900

    test target 추가 및 scheme 수정

---
+++ BlackjackTest/BlackjackTest.swift
@@ -0,0 +1,17 @@
+//
+//  BlackjackTest.swift
+//  BlackjackTest
+//
+//  Created by 남기범 on 2021/11/15.
+//
+
+import XCTest
+
+class BlackjackTest: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+}
