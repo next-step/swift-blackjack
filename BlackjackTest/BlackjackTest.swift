d9fa4da8fe72b73915fa4ee45e1538635dd9c3cb
BeomKey <nkb5813@naver.com>
2021-11-20 18:39:06 +0900

    카드 종류에 따른 점수 합산 결과 테스트케이스 추가

--- BlackjackTest/BlackjackTest.swift
+++ BlackjackTest/BlackjackTest.swift
@@ -6,20 +6,38 @@
 //
 
 import XCTest
+@testable import Blackjack
 
 class BlackjackTest: XCTestCase {
+    var gammer: CardGammer!
 
     override func setUpWithError() throws {
+        gammer = CardGammer()
     }
 
     override func tearDownWithError() throws {
+        gammer = nil
     }
     
     func test_gamer_card_sum_have_nomal_card() {
-        let gammer = CardGammer()
         gammer.pickCard(Card(number: 3, type: .hart))
         gammer.pickCard(Card(number: 5, type: .spade))
         
         XCTAssertEqual(8, gammer.cardSum)
+    }
+    
+    func test_gamer_card_sum_have_king_queen_jack() {
+        gammer.pickCard(Card(number: nil, type: .jack))
+        gammer.pickCard(Card(number: nil, type: .king))
+        gammer.pickCard(Card(number: nil, type: .queen))
+        
+        XCTAssertEqual(30, gammer.cardSum)
+    }
+    
+    func test_gamer_card_sum_have_ace() {
+        gammer.pickCard(Card(number: nil, type: .ace))
+        gammer.pickCard(Card(number: nil, type: .jack))
+        
+        XCTAssertEqual(21, gammer.cardSum)
     }
 }
