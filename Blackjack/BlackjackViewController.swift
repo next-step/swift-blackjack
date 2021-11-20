6aeee83011a1088b7dce14e15ae31daae114de66
BeomKey <nkb5813@naver.com>
2021-11-20 22:05:57 +0900

    inputView 구현

--- Blackjack/BlackjackViewController.swift
+++ Blackjack/BlackjackViewController.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+struct BlackjackInputView {
+    enum InputError: Error {
+        case notInputYorN
+    }
+    
+    func inputGammerName() -> String {
+        print("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)")
+        return readLine() ?? ""
+    }
+
+    func inputIsPick(name: String) throws -> Bool {
+        print("\(name)는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n")
+        guard let input = readLine() else { throw InputError.notInputYorN }
+        
+        switch input {
+        case "y": return true
+        case "n": return false
+        default: throw InputError.notInputYorN
+        }
+    }
+}
