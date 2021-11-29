1746e33ebf50e1357b43bc194b84ed2cbd66f4d4
Mephrine <dthwrld@gmail.com>
2021-11-29 10:56:57 +0900

    refactor: Add computed properties used externally

--- Blackjack/Entities/Deck.swift
+++ Blackjack/Entities/Deck.swift
@@ -11,8 +11,29 @@ struct Deck: Equatable {
 	static func == (lhs: Deck, rhs: Deck) -> Bool {
 		lhs.cards == rhs.cards
 	}
+	private(set) var cards: [BlackjackCard]
 	
-	var cards: [BlackjackCard]
+	var sumOfCardNumbers: Int {
+		CardScoreCalculator.calculateTheSumOfCardNumbers(in: self)
+	}
+	
+	var isBust: Bool {
+		sumOfCardNumbers > BlackjackOption.blackjackNumber
+	}
+	
+	var isBlackjack: Bool {
+		sumOfCardNumbers == BlackjackOption.blackjackNumber
+	}
+	
+	var hasAceCard: Bool {
+		cards.filter { card in
+			card.rank == .ace
+		}.count > 0
+	}
+	
+	var description: String {
+		cards.map { $0.description }.joined(separator: ", ")
+	}
 	
 	mutating func draw(of card: BlackjackCard) {
 		self.cards.append(card)
