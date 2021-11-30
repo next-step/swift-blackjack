bb57b314e02883b42c4b73de42e02265d4ca2312
Mephrine <dthwrld@gmail.com>
2021-11-30 16:35:46 +0900

    style: Remove unused codes

--- Blackjack/Entities/Winning.swift
+++ Blackjack/Entities/Winning.swift
@@ -11,19 +11,4 @@ enum Winning {
 	case win
 	case push
 	case lose
-	
-	func description(by count: Int) -> String {
-		return "\(count)\(self.description)"
-	}
-	
-	var description: String {
-		switch self {
-		case .win:
-			return "승"
-		case .push:
-			return "무"
-		case .lose:
-			return "패"
-		}
-	}
 }
