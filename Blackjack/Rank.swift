9b5e3ef1de70f61e6a74a6ef66fd3db6cfaeed22
mansa.kim <zombietux@naver.com>
2022-05-04 22:05:44 +0900

    feat: Rank 개체 추가

---
+++ Blackjack/Rank.swift
@@ -0,0 +1,29 @@
+//
+//  Rank.swift
+//  Blackjack
+//
+//  Created by 카카오VX on 2022/05/04.
+//
+
+import Foundation
+
+enum Rank: Int, CustomStringConvertible, CaseIterable {
+    case two = 2, three, four, five, six, seven, eight, nine, ten
+    case A, J, Q, K
+    
+    var description: String {
+        switch self {
+        case .A: return "A"
+        case .J: return "J"
+        case .Q: return "Q"
+        case .K: return "K"
+        default: return String(self.rawValue)
+        }
+    }
+}
+
+extension Rank: Comparable {
+    static func <(lhs: Rank, rhs: Rank) -> Bool {
+        return lhs.rawValue < rhs.rawValue
+    }
+}
