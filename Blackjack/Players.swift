160f10f975a3689afccdcdec490b8ff6fffb40fa
mansa.kim <zombietux@naver.com>
2022-05-08 01:00:57 +0900

    feat: 플레이어들의 행동을 담당하는 Players개체 추가

---
+++ Blackjack/Players.swift
@@ -0,0 +1,66 @@
+//
+//  Players.swift
+//  Blackjack
+//
+//  Created by 카카오VX on 2022/05/05.
+//
+
+import Foundation
+
+protocol PlayersProtocol {
+    func handOutFirstHand()
+    func forEachPlayers(behavior: (Playable) -> ())
+    func turnForPlayer(behavior: (Playable) -> ())
+    func turnToPlayer() -> Playable?
+}
+
+class Players: PlayersProtocol {
+    private var players: [Playable] = []
+    private let cardDeck: CardDeckProtocol
+    private lazy var turnToHit: Playable? = players.first
+    
+    init?(with names: [String], cardDeck: CardDeckProtocol) {
+        self.cardDeck = cardDeck
+        guard names.count > 0 || cardDeck.count() > 0 else { return nil }
+        names.forEach { name in
+            let participant = Participant(name: name)
+            players.append(participant)
+        }
+    }
+    
+    func handOutFirstHand() {
+        players.forEach { player in
+            guard let hand = cardDeck.makeHand() else { return }
+            player.takeAFirstHand(hand)
+        }
+    }
+    
+    func forEachPlayers(behavior: (Playable) -> ()) {
+        players.forEach(behavior)
+    }
+    
+    func turnToPlayer() -> Playable? {
+        turnToHit
+    }
+    
+    //FIXME: - 리팩터링 필요
+    func turnForPlayer(behavior: (Playable) -> ()) {
+        players.forEach { player in
+            var currentTurnToPlayer: Playable? = nil
+
+            if turnToHit?.giveIsHit() == true && turnToHit?.giveName() == player.giveName() {
+                turnToHit?.hit(card: cardDeck.handOutCard())
+                return behavior(player)
+            }
+
+            guard turnToHit?.giveIsHit() == true else {
+                turnToHit = player
+                return
+            }
+
+            guard let turnPlayer = currentTurnToPlayer else { return }
+            currentTurnToPlayer = player
+            behavior(turnPlayer)
+        }
+    }
+}
