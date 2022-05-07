d072aa92237be301d5b635a6227c1dd3bf841ec6
mansa.kim <zombietux@naver.com>
2022-05-08 00:55:23 +0900

    feat: 참가자 입력을 구분하기 위한 개체 추가

---
+++ Blackjack/StringUtiltity.swift
@@ -0,0 +1,14 @@
+//
+//  StringUtiltity.swift
+//  Blackjack
+//
+//  Created by 카카오VX on 2022/05/06.
+//
+
+import Foundation
+
+struct StringUtiltity {
+    static func splitParticipantsName(to convertedExpression: String) -> [String] {
+        convertedExpression.components(separatedBy: ",")
+    }
+}
