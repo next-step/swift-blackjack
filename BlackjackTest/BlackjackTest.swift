47116acb1547143792642a75ce6a5045d0309168
mansa.kim <zombietux@naver.com>
2022-05-08 01:01:15 +0900

    feat: Blackjack 테스트 추가

--- BlackjackTest/BlackjackTest.swift
+++ BlackjackTest/BlackjackTest.swift
@@ -9,10 +9,20 @@ import XCTest
 
 class BlackjackTest: XCTestCase {
     func testBlackjack_make_success() {
+        let cardDeck = CardDeck(cards: CardDeckGenerator.generate())!
+        let players = Players(with: ["mansa","zombie"], cardDeck: cardDeck)
+        let game = BlackjackGame(players: players!)
         
+        XCTAssertNotNil(game)
     }
     
-    func testBlackjack_make_fail() {
+    func testBlackjack_handOutFirstHand() {
+        let cardDeck = CardDeck(cards: CardDeckGenerator.generate())!
+        let players = Players(with: ["mansa","zombie"], cardDeck: cardDeck)
+        let game = BlackjackGame(players: players!)
+        game.handOutFirstHand()
+        guard let turnToPlayer = players?.turnToPlayer() else { return }
         
+        XCTAssertTrue(turnToPlayer.score() > 2)
     }
 }
