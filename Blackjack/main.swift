f6449a8fff4bc51d9a2670311ac66fa8ab7b2a1b
mansa.kim <zombietux@naver.com>
2022-05-08 01:20:27 +0900

    feat: 블랙잭 게임 메인함수 추가

--- Blackjack/main.swift
+++ Blackjack/main.swift
@@ -6,5 +6,36 @@
 
 import Foundation
 
-print("Hello, World!")
+func main() {
+    let rawParticipansts = InputView.readParticipants()
+    let nameOfParticipants = StringUtiltity.splitParticipantsName(to: rawParticipansts)
+    guard let cardDeck = CardDeck(cards: CardDeckGenerator.generate()) else { return }
+    cardDeck.shuffle()
+    guard let players = Players(with: nameOfParticipants, cardDeck: cardDeck) else { return }
+    
+    let game = BlackjackGame(players: players)
+    let controller = BlackjackGameContrller(with: game)
+    
+    ResultView.printGameStart(nameOfParticipants: rawParticipansts)
+    controller.noticeHandOfPlayers { participant in
+        ResultView.printPlayer(player: participant)
+    }
+    
+    while game.isPlayingGame() {
+        guard let turnToPlayer = game.turnToPlayer() else { return }
+        let hitOrStay = InputView.readAskHitToParticipants(to: turnToPlayer)
+        turnToPlayer.hitOrStay(hitOrStay)
+
+        game.turnForPlayer { turnToPlayer in
+            ResultView.printPlayer(player: turnToPlayer)
+        }
+    }
+    
+    controller.noticeResultOfGame { participant in
+        ResultView.printGameResult(player: participant)
+    }
+}
+
+main()
+
 
