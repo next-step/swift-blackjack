fad3bc3d3b2b2bc821584c78727899ede37b118f
mansa.kim <zombietux@naver.com>
2022-05-10 08:11:59 +0900

    feat: Players에 딜러 추가 후 승패 관련 동작 Foreach 메서드 추가

--- Blackjack/Players.swift
+++ Blackjack/Players.swift
@@ -12,20 +12,24 @@ protocol PlayersProtocol {
     func forEachPlayers(behavior: (Playable) -> ())
     func turnForPlayer(behavior: (Playable) -> ())
     func turnToPlayer() -> Playable?
+    func winOrLoseForPlayer(behavior: (Playable) -> ())
+    func turnTodealer() -> Playable?
 }
 
 class Players: PlayersProtocol {
     private var players: [Playable] = []
     private let cardDeck: CardDeckProtocol
     private lazy var turnToHit: Playable? = players.first
+    private lazy var dealer: Playable? = players.last
     
-    init?(with names: [String], cardDeck: CardDeckProtocol) {
+    init?(with names: [String], with dealer: Playable = Dealer(), cardDeck: CardDeckProtocol) {
         self.cardDeck = cardDeck
         guard names.count > 0 || cardDeck.count() > 0 else { return nil }
         names.forEach { name in
             let participant = Participant(name: name)
             players.append(participant)
         }
+        players.append(dealer)
     }
     
     func handOutFirstHand() {
@@ -41,6 +45,24 @@ class Players: PlayersProtocol {
     
     func turnToPlayer() -> Playable? {
         turnToHit
+    }
+    
+    func turnTodealer() -> Playable? {
+        dealer
+    }
+    
+    func winOrLoseForPlayer(behavior: (Playable) -> ()) {
+        players.forEach { player in
+            guard player.giveName() != dealer?.giveName() else { return behavior(player) }
+            guard player.score() >= dealer?.score() ?? 0 else {
+                player.record(.lose)
+                dealer?.record(.win)
+                return behavior(player)
+            }
+            player.record(.win)
+            dealer?.record(.lose)
+            behavior(player)
+        }
     }
     
     //FIXME: - 리팩터링 필요
