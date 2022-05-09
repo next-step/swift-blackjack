8c2bb9075b6ebb8aa7840296b55fe3c59591dcac
mansa.kim <zombietux@naver.com>
2022-05-10 08:13:37 +0900

    feat: 컨트롤러 승패 관련 메서드 추가

--- Blackjack/BlackjackGameController.swift
+++ Blackjack/BlackjackGameController.swift
@@ -18,7 +18,54 @@ struct BlackjackGameContrller {
         game.forEachPlayers(behavior: gameHandler)
     }
     
+    func noticeTurnOfGame(gameHandler: (Playable) -> ()) {
+        while game.isPlayingGame() {
+            turn(behavior: gameHandler)
+        }
+    }
+    
+    private func turn(behavior: (Playable) -> ()) {
+        if isDealer() {
+            turnForDealer()
+        } else {
+            turnForParticipant()
+        }
+        
+        game.turnForPlayer { turnToPlayer in
+            behavior(turnToPlayer)
+        }
+    }
+    
+    private func turnForParticipant() {
+        guard let turnToPlayer = game.turnToPlayer() else { return }
+        
+        let hitOrStay = InputView.readAskHitToParticipants(to: turnToPlayer)
+        turnToPlayer.hitOrStay(hitOrStay)
+    }
+    
+    private func turnForDealer() {
+        guard let turnToPlayer = game.turnToPlayer() else { return }
+        
+        if turnToPlayer.score() <= 17 {
+            turnToPlayer.hitOrStay(true)
+            ResultView.printHitDlear()
+        } else {
+            turnToPlayer.hitOrStay(false)
+        }
+    }
+    
+    private func isDealer() -> Bool {
+        guard let turnToPlayer = game.turnToPlayer() else { return false }
+        guard let delar = game.dealer() else { return false }
+        
+        return turnToPlayer.giveName() == delar.giveName()
+    }
+    
     func noticeResultOfGame(gameHandler: (Playable) -> ()) {
         game.forEachPlayers(behavior: gameHandler)
+    }
+    
+    func noticeWinnerOfGame(gameHandler: (Playable) -> ()) {
+        game.winOrLoseForPlayer(behavior: gameHandler)
     }
 }
