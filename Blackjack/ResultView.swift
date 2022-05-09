5fad953cd4527d77dff19bdc155b09a90f788139
mansa.kim <zombietux@naver.com>
2022-05-10 08:14:17 +0900

    feat: ResultView print메서드 추가

--- Blackjack/ResultView.swift
+++ Blackjack/ResultView.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct ResultView {
     static func printGameStart(nameOfParticipants: String) {
-        print("\(nameOfParticipants)에게 2장씩 나누었습니다.")
+        print("딜러와 \(nameOfParticipants)에게 2장씩 나누었습니다.")
     }
     
     static func printPlayer(player: Playable) {
@@ -18,5 +18,17 @@ struct ResultView {
     
     static func printGameResult(player: Playable) {
         print("\(player.giveName())의 카드: \(player.giveHandDescription()) - 결과: \(player.score())")
+    }
+    
+    static func printHitDlear() {
+        print("딜러는 16이하라 한장의 카드를 더 받았습니다.")
+    }
+    
+    static func printWinOrLoseTitle() {
+        print("## 최종 승패")
+    }
+    
+    static func printWinOrLose(player: Playable) {
+        print("\(player.giveName()): \(player.giveWinLoseRecord())")
     }
 }
