2fff15ef79c4f363742a6a8399724d05030e6aaf
sujin <rkdthd1234@naver.com>
2022-05-22 19:55:54 +0900

    1단계. InputView 작성

---
+++ Blackjack/View/InputView.swift
@@ -0,0 +1,44 @@
+//
+//  InputView.swift
+//  Blackjack
+//
+//  Created by 강수진 on 2022/05/22.
+//
+
+import Foundation
+
+struct InputView {
+    
+    private enum QuestionText: UserInformable {
+        case gamerNames
+        case hitOrStay(gamer: Gamer)
+        
+        var guideDescription: String {
+            switch self {
+            case .gamerNames:
+                return "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
+            case let .hitOrStay(gamer):
+                return "\(gamer.name)는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)"
+            }
+        }
+    }
+    
+    private let userGuider = UserGuider()
+    private let userInputConverter = UserInputConverter()
+    
+    func receiveGamers() -> [Gamer] {
+        userGuider.printGuide(for: QuestionText.gamerNames)
+        
+        let userInput: String? = readLine()
+        let gamers: [Gamer] = userInputConverter.convertToGamers(from: userInput)
+        return gamers
+    }
+    
+    func receiveToHitOrStay(of gamer: Gamer) throws -> Gamer.State {
+        userGuider.printGuide(for: QuestionText.hitOrStay(gamer: gamer))
+        
+        let userInput: String? = readLine()
+        let gamerState: Gamer.State = try userInputConverter.convertToHitOrStay(from: userInput)
+        return gamerState
+    }
+}
