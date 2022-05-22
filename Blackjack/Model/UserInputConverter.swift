598efea40d88e89ad65b9b43e0317d3237097ecc
sujin <rkdthd1234@naver.com>
2022-05-22 17:39:15 +0900

    1단계. UserInputConverter 작성

--- Blackjack/Model/UserInputConverter.swift
+++ Blackjack/Model/UserInputConverter.swift
@@ -8,13 +8,50 @@
 import Foundation
 
 struct UserInputConverter {
-
+    
+    enum UserInputConverterError: LocalizedError {
+        case invalidCardReceiveDecision
+        
+        var errorDescription: String? {
+            switch self {
+            case .invalidCardReceiveDecision:
+                return "y 나 n 를 입력해주세요"
+            }
+        }
+    }
+    
     func convertToGamers(from input: String?) -> [Gamer] {
-        return []
+        guard let input = input,
+              !input.isEmpty else {
+                  return []
+              }
+        
+        let separator: String = ","
+        let gamers: [Gamer] = input.components(separatedBy: separator)
+            .map { name in
+                name.trimmingCharacters(in: .whitespaces)
+            }
+            .map { name in
+                Gamer(name: name)
+            }
+        return gamers
     }
     
     func convertToIsReceiveCard(from input: String?) throws -> Bool {
-        return false
+        
+        guard let input = input,
+              !input.isEmpty else {
+                  throw UserInputConverter.UserInputConverterError.invalidCardReceiveDecision
+              }
+        
+        switch input {
+        case "y":
+            return true
+        case "n":
+            return false
+        default:
+            throw UserInputConverter.UserInputConverterError.invalidCardReceiveDecision
+        }
     }
 }
 
