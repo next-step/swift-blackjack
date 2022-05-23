851553e52766465c97ba0cbd959b8e1aa74a4ade
RoadtoS7 <kshvv975@gmail.com>
2022-05-23 23:57:15 +0900

    feat: CardDeck에 카드를 추가하는 메서드 유닛 테스트 케이스 추가

---
+++ Blackjack/CardDeck.swift
@@ -0,0 +1,8 @@
+//
+//  CardDeck.swift
+//  Blackjack
+//
+//  Created by nylah.j on 2022/05/23.
+//
+
+import Foundation
---
+++ BlackjackTest/CardDeckTest.swift
@@ -0,0 +1,24 @@
+//
+//  CardDeckTest.swift
+//  BlackjackTest
+//
+//  Created by nylah.j on 2022/05/23.
+//
+
+import XCTest
+
+class CardDeckTest: XCTestCase {
+
+    func test_append_추가한카드를_보관한다() {
+        // given
+        let cardDeck = BlackjackCardDeck()
+        let card = Card(id: .king, symbol: .diamond)
+        
+        // when
+        cardDeck.append(card: card)
+        
+        // then
+        XCTAssertTrue(cardDeck.cards.contains(card))
+    }
+
+}
