5da9de04beb0ea2d9fa664fa49d2c8c65ccc00e0
RoadtoS7 <kshvv975@gmail.com>
2022-05-23 23:41:46 +0900

    feat: PlayerName이 콤마를 입력으로 받으면 nil을 반환하는지 체크하는 테스트 케이스 추가

---
+++ BlackjackTest/PlayerName.swift
@@ -0,0 +1,14 @@
+//
+//  PlayerName.swift
+//  BlackjackTest
+//
+//  Created by nylah.j on 2022/05/23.
+//
+
+import XCTest
+
+class PlayerName: XCTestCase {
+    func test_콤마는_playername으로_가질수없다() {
+        XCTAssertNil(PlayerName(","))
+    }
+}
