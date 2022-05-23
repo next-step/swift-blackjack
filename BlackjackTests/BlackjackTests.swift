e56875b3bda66ba5fa063877913c32e0a5ad2cf8
YuchanSong <uchanism24@naver.com>
2022-05-23 18:58:24 +0900

    test: 덱 카드 수 검증 테스트 코드 작성

--- BlackjackTests/BlackjackTests.swift
+++ BlackjackTests/BlackjackTests.swift
@@ -8,5 +8,14 @@
 import XCTest
 
 class BlackjackTests: XCTestCase {
-    
+    func test_덱의_카드수가_52장인지_검증() throws {
+        // given
+        let deck: Deck = Deck()
+        
+        // when
+        let deckCount: Int = 52
+        
+        // then
+        XCTAssertTrue(deckCount == deck.count)
+    }
 }
