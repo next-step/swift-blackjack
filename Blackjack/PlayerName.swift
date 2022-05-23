3e9b373565e0f643829a9791d6b790c8cd964105
RoadtoS7 <kshvv975@gmail.com>
2022-05-23 23:46:38 +0900

    feat: PlayerName 리팩터링

--- Blackjack/PlayerName.swift
+++ Blackjack/PlayerName.swift
@@ -8,10 +8,11 @@
 import Foundation
 
 struct PlayerName {
+    private let comma = ","
     let value: String
     
     init?(_ value: String) {
-        if value.contains(",") { return nil }
+        if value.contains(comma) { return nil }
         self.value = value
     }
 }
