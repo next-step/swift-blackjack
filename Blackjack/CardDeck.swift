00b8a37bc9f12c1508ae204188bda4fabeaecf68
RoadtoS7 <kshvv975@gmail.com>
2022-05-24 12:21:42 +0900

    feat: CardDeck.countScore 구현

--- Blackjack/CardDeck.swift
+++ Blackjack/CardDeck.swift
@@ -9,13 +9,20 @@ import Foundation
 
 protocol CardDeck {
     var cards: [Card] { get }
+    var cardScoreRule: CardScoreRule.Type { get }
     func append(card: Card)
+    func countScore() -> Int
 }
 
 class BlackjackCardDeck: CardDeck {
     private(set) var cards: [Card] = []
+    var cardScoreRule: CardScoreRule.Type = BlackjackScoreRule.self
 
     func append(card: Card) {
         cards.append(card)
+    }
+    
+    func countScore() -> Int {
+        return cardScoreRule.countScore(cardDeck: self)
     }
 }
