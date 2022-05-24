e379ca3d459e3a7e67b2aaff0583110e520c18b8
YuchanSong <uchanism24@naver.com>
2022-05-24 21:33:11 +0900

    feat: main 클래스 작성

--- Blackjack/main.swift
+++ Blackjack/main.swift
@@ -4,41 +4,16 @@
 //  Copyright © yagom. All rights reserved.
 //
 
-protocol Playable {
-    func add(_ element: Card)
-}
-
-class Player: Playable {
-    let name: String
-    var cards: Cards
-    
-    init(name: String, _ cards: Cards = Cards()) {
-        self.name = name
-        self.cards = cards
-    }
-    
-    func add(_ element: Card) {
-        cards.append(element)
-    }
-}
-
-var deck = Deck()
-let player1: Player = Player(name: "uchan")
-let player2: Player = Player(name: "hui")
-
-for _ in 1...2 {
-    player1.add(deck.deal())
-}
-
-for _ in 1...2 {
-    player2.add(deck.deal())
-}
-
-print(player1.cards)
-print(player2.cards)
-
-//print("\(player1.name): \(player1.cards.sum)")
-//print("\(player2.name): \(player2.cards.sum)")
-
 import Foundation
 
+do {
+    let players = try InputView.readUserNames()
+    let game = BlackjackGame(players: players)
+    try game.startGame()
+} catch let error as CardError {
+    print(error)
+} catch let error as InputError {
+    print(error)
+} catch let error {
+    print(error.localizedDescription)
+}
