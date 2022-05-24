638da520e8aa246c6dba5c9c6de31e407666fa80
RoadtoS7 <kshvv975@gmail.com>
2022-05-24 12:19:27 +0900

    feat: CardDeck에 존재하는 카드의 합계점수를 계산하는 countScore 유닛테스트 구현

--- BlackjackTest/CardDeckTest.swift
+++ BlackjackTest/CardDeckTest.swift
@@ -21,5 +21,20 @@ class CardDeckTest: XCTestCase {
         // then
         XCTAssertTrue(cardDeck.cards.contains(card))
     }
-
+    
+    func test_countScore_자신이_가진_cardScoreRule에_따라서_카드점수를_집계한다() {
+        // given
+        let cardDeck = BlackjackCardDeck()
+        let firstCard = Card(id: .king, symbol: .diamond)
+        let secondCard = Card(id: .ace, symbol: .heart)
+        
+        cardDeck.append(card: firstCard)
+        cardDeck.append(card: secondCard)
+        
+        // when
+        let score = cardDeck.countScore()
+        
+        // then
+        XCTAssertEqual(score, 21)
+    }
 }
