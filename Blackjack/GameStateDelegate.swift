ce01d3ca39ec7ed4d115e209ba48cc5fbebb55b4
RoadtoS7 <kshvv975@gmail.com>
2022-05-24 14:19:59 +0900

    feat: 게임의 각 단계 상황을 출력하는 역할을 하는 GameStateDelegate 구현

---
+++ Blackjack/GameStateDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  BlackjackStateDelegate.swift
+//  Blackjack
+//
+//  Created by nylah.j on 2022/05/24.
+//
+
+import Foundation
+
+protocol GameStateDelegate {
+    func afterInit(players: [Player])
+    func afterReceiveCard(player: Player)
+}
