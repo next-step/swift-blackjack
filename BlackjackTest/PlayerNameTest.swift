76dd623344c2bfea395c6fd8e3efec644cd9e05c
RoadtoS7 <kshvv975@gmail.com>
2022-05-25 23:03:38 +0900

    refactor: PlayerName 유닛테스트 케이스 이름 컨벤션을 다른 유닛테스트와 일치시킴

--- BlackjackTest/PlayerNameTest.swift
+++ BlackjackTest/PlayerNameTest.swift
@@ -8,7 +8,7 @@
 import XCTest
 
 class PlayerNameTest: XCTestCase {
-    func test_콤마는_playername으로_가질수없다() {
+    func test_콤마를_이용하여_PlayerName을_생성하면_nil을_반환한다() {
         XCTAssertNil(PlayerName(","))
     }
 }
