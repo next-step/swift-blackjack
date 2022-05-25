9618b309d7118a066745e58ed278178b0461d855
sujin <rkdthd1234@naver.com>
2022-05-26 01:24:31 +0900

    ResultView 작성

--- Blackjack/View/ResultView.swift
+++ Blackjack/View/ResultView.swift
@@ -11,17 +11,30 @@ struct ResultView {
     
     private enum ResultText: UserInformable {
         case score(gamer: Gamer)
+        case finalOutcome
+        case dealerOutcome(description: String)
+        case gamerOutcome(name: String, description: String)
         
         var guideDescription: String {
             switch self {
             case let .score(gamer):
                 return "\(gamer.name)카드: \(gamer.cardsDescription) - 결과: \(gamer.totalPoint)"
+            case .finalOutcome:
+                return "## 최종 승패"
+            case let .dealerOutcome(description):
+                return "딜러 \(description)"
+            case let .gamerOutcome(name, description):
+                return "\(name): \(description)"
             }
         }
     }
+
     
     private let userGuider = UserGuider()
+    private var gameResult: GameResult
     
+    init(gameResult: GameResult) {
+        self.gameResult = gameResult
     }
     
     func printScore(of gamers: [Gamer]) {
@@ -30,5 +43,27 @@ struct ResultView {
     
     private func printScore(of gamer: Gamer) {
         userGuider.printGuide(for: ResultText.score(gamer: gamer))
+    }
+    
+    func printFinalOutcome(with gamers: [Gamer]) {
+        userGuider.printGuide(for: ResultText.finalOutcome)
+        
+        printDealerOutcome()
+        printGamersOutcome(gamers)
+    }
+    
+    private func printDealerOutcome() {
+        let description: String = gameResult.dealerDescription
+        userGuider.printGuide(for: ResultText.dealerOutcome(description: description))
+    }
+    
+    private func printGamersOutcome(_ gamers: [Gamer]) {
+        gamers.forEach(printGamerOutcome)
+    }
+    
+    private func printGamerOutcome(_ gamer: Gamer) {
+        let description: String = gameResult.gamerDescription(gamer)
+        userGuider.printGuide(for: ResultText.gamerOutcome(name: gamer.name,
+                                                           description: description))
     }
 }
