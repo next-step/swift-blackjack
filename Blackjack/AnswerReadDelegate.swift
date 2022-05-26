8c3e53e18a703f25e337a0ebb1d35a76a82a6e76
RoadtoS7 <kshvv975@gmail.com>
2022-05-26 23:41:19 +0900

    feat: BlackjackAnswerReader가 카드 점수가 16이하인 딜러의 응답으로 yes를 반환하도록 구현

--- Blackjack/AnswerReadDelegate.swift
+++ Blackjack/AnswerReadDelegate.swift
@@ -13,6 +13,10 @@ protocol ReadAnswerDelegate {
 
 struct BlackjackAnswerReader: ReadAnswerDelegate {
     func readAnswer(player: Player) throws -> Answer {
+        if let dealer = player as? Dealer {
+            let score = dealer.countScore()
+            return score <= 16 ? .yes: .no
+        }
         let answerInput = InputView.readAnswer(from: player)
         let answer = try AnswerParser.parse(answerInput: answerInput)
         return answer
