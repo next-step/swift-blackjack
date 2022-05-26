8b25a3541f1eedaaa6219f5630e179db0ecfaf16
RoadtoS7 <kshvv975@gmail.com>
2022-05-26 18:55:05 +0900

    refactor: NonDuplicateCardDistributor를 테스트할 때 테스트 대상에 카드를 한장만 두도록 수정
    뽑을 카드 집합의 카드 장수가 1장일 때와 여러장일 때 테스트 케이스의 의미가 없으며
    오히려 1장밖에 없을 때 카드를 뽑는 것이 더 엣지 케이에 가깝다

--- BlackjackTest/CardDistributorTest.swift
+++ BlackjackTest/CardDistributorTest.swift
@@ -20,10 +20,9 @@ class CardDistributorTest: XCTestCase {
     func test_NonDuplicateCardDistributor_뽑은카드는_더이상_CardDistributor에서_앞으로_반환할_카드_대상에_포함되지않는다() throws {
         // given
         let firstCard = Card(rank: .ace, suit: .heart)
-        let secondCard = Card(rank: .ace, suit: .diamond)
         let stubCardPickStrategy = StubCardPickStrategy(card: firstCard)
         
-        let cardDistributor = NonDuplicateCardDistributor(cards: [firstCard, secondCard], cardPickStrategy: stubCardPickStrategy)
+        let cardDistributor = NonDuplicateCardDistributor(cards: [firstCard], cardPickStrategy: stubCardPickStrategy)
     
         // when
         let _ = try cardDistributor.distribute(count: 1)
