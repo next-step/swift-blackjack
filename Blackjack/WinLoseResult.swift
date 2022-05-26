bd47374aff68e4de61ba31fffb1611d618cedcb4
RoadtoS7 <kshvv975@gmail.com>
2022-05-26 23:55:13 +0900

    feat: WinLoseResult의 failable intializer 구현
    WinLoseResult는 winCount, loseCount로 양수만 가질 수 있다. 만일 intializer로 음수가 들어온다면 nil을 반환한다

--- Blackjack/WinLoseResult.swift
+++ Blackjack/WinLoseResult.swift
@@ -10,4 +10,10 @@ import Foundation
 struct WinLoseResult{
     let winCount: Int
     let loseCount: Int
+    
+    init?(winCount: Int, loseCount: Int) {
+        guard winCount > 0, loseCount > 0 else { return nil }
+        self.winCount = winCount
+        self.loseCount = loseCount
+    }
 }
