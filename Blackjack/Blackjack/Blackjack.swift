ed0ac9e37e1c97f604475501b8707262ce1fdb22
woosub-lee <woosub.dev@gmail.com>
2022-05-26 21:57:17 +0900

    feat: blackjack participants

---
+++ Blackjack/Blackjack/Blackjack.swift
@@ -0,0 +1,16 @@
+//
+//  Blackjack.swift
+//  Blackjack
+//
+//  Created by 이우섭 on 2022/05/26.
+//
+
+import Foundation
+
+struct Blackjack {
+    var participants: [String]
+    
+    init(participantNames: [String]) {
+        self.participants = participantNames
+    }
+}
