f87638c7a8f2b0f8aea2e7577f58cbe65655312a
RoadtoS7 <kshvv975@gmail.com>
2022-05-26 23:59:27 +0900

    refactor: WinLoseResult가 어떤 Player의 승패결과인지 알 수 있도록 Player을 프로퍼티로 갖도록 수정

--- BlackjackTest/WinLoseResultTest.swift
+++ BlackjackTest/WinLoseResultTest.swift
@@ -10,10 +10,20 @@ import XCTest
 class WinLoseResultTest: XCTestCase {
 
     func test_WinLoseResult는_winCount로_양수만_갖는다() {
-        XCTAssertNil(WinLoseResult(winCount: -1, loseCount: 0))
+        // given
+        let player = Player(name: PlayerName("player")!, cardDeck: BlackjackCardDeck())
+        
+        // when
+        // then
+        XCTAssertNil(WinLoseResult(player: player, winCount: -1, loseCount: 0))
     }
     
     func test_WinLoseResult는_loseCount로_양수만_갖는다() {
-        XCTAssertNil(WinLoseResult(winCount: 0, loseCount: -1))
+        // given
+        let player = Player(name: PlayerName("player")!, cardDeck: BlackjackCardDeck())
+        
+        // when
+        // then
+        XCTAssertNil(WinLoseResult(player: player, winCount: 0, loseCount: -1))
     }
 }
