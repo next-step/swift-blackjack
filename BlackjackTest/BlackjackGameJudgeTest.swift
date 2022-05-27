2dbf7aaab622d060077876ceda8bfb8685b54099
RoadtoS7 <kshvv975@gmail.com>
2022-05-27 15:56:53 +0900

    feat: 특정 참가자의 점수를 기준으로 참가자들의 승패 결과를 계산하는 BlackjackJudge.winLoseResults() 유닛 테스트 구현

--- BlackjackTest/BlackjackGameJudgeTest.swift
+++ BlackjackTest/BlackjackGameJudgeTest.swift
@@ -19,6 +19,31 @@ class BlackjackGameJudgeTest: XCTestCase {
         
         // when
         let winLoseResult = gameJudge.winLoseResult(of: playerScore, comparingWith: dealerScore)
+        
+        // then
         XCTAssertEqual(winLoseResult.winCount, 1)
+    }
+    
+    func test_winLoseResultScores_게임참가자들의_게임점수를_가지고_실패결과에_해당하는_WinLoseResults를_반환한다() {
+        // given
+        let gameJudge = BlackjackGameJudge()
+        let dealer = Dealer(cardDeck: BlackjackCardDeck())
+        let playerOne = Player(name: PlayerName("kim")!, cardDeck: BlackjackCardDeck())
+        let playerTwo = Player(name: PlayerName("lee")!, cardDeck: BlackjackCardDeck())
+        
+        let dealerScore = BlackjackScore(player: dealer, score: 21)
+        let playerOneScore = BlackjackScore(player: playerOne, score: 21)
+        let playerTwoScore = BlackjackScore(player: playerTwo, score: 20)
+        
+        let dealerResult = WinLoseResult(player: dealer, winCount: 1, loseCount: 1)
+        let playerOneResult = WinLoseResult(player: playerOne, winCount: 1, loseCount: 0)
+        let playerTwoResult = WinLoseResult(player: playerTwo, winCount: 0, loseCount: 1)
+            // when
+        let winLoseResults = gameJudge.winLoseResults(with: [playerOneScore, playerTwoScore], comparingWith: dealerScore)
+        
+        // then
+        XCTAssertEqual(winLoseResults[dealer], dealerResult)
+        XCTAssertEqual(winLoseResults[playerOne], playerOneResult)
+        XCTAssertEqual(winLoseResults[playerTwo], playerTwoResult)
     }
 }
