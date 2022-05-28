160eddaed851e8a9d41c3c9686651e34d07d5d79
RoadtoS7 <kshvv975@gmail.com>
2022-05-28 19:03:22 +0900

    refactor: WinLoseResult에서 zero 상수 대신 Int.zero를 사용하도록 수정

--- Blackjack/WinLoseResult.swift
+++ Blackjack/WinLoseResult.swift
@@ -8,14 +8,12 @@
 import Foundation
 
 struct WinLoseResult {
-    private let zero = 0
-    
     let player: Player
     let winCount: Int
     let loseCount: Int
     
     init?(player: Player, winCount: Int, loseCount: Int) {
-        guard winCount >= zero, loseCount >= zero else { return nil }
+        guard winCount >= Int.zero, loseCount >= Int.zero else { return nil }
         self.player = player
         self.winCount = winCount
         self.loseCount = loseCount
