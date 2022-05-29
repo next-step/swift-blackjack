d5c903846e19ba0b2701ee6d0aa76896eaf81cd3
RoadtoS7 <kshvv975@gmail.com>
2022-05-29 13:48:31 +0900

    refactor: WinLoseResult에서 Int.zero -> .zero로 줄임

--- Blackjack/WinLoseResult.swift
+++ Blackjack/WinLoseResult.swift
@@ -13,7 +13,7 @@ struct WinLoseResult {
     let loseCount: Int
     
     init?(player: Player, winCount: Int, loseCount: Int) {
-        guard winCount >= Int.zero, loseCount >= Int.zero else { return nil }
+        guard winCount >= .zero, loseCount >= .zero else { return nil }
         self.player = player
         self.winCount = winCount
         self.loseCount = loseCount
