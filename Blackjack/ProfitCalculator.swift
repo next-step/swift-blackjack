f89b90d202df4f72adbcb7103e85b973d6f4b81c
RoadtoS7 <kshvv975@gmail.com>
2022-05-30 01:29:24 +0900

    feat: ProfitCalculator 딜러가_블랙잭이_아닐때_블랙잭_player는_배팅금액의_일점오배를_수익으로_얻는다 기능 구현

--- Blackjack/ProfitCalculator.swift
+++ Blackjack/ProfitCalculator.swift
@@ -43,8 +43,36 @@ struct ProfitCalculator {
             return Profits(value: [dealerProfit] + zeroProfits + minusProfits)
         }
         
+        let nonBlackjackScores = scores.value.filter { nonBlackjackPlayers.contains($0.player) }
         let blackjackProfits = blackjackProfits(of: blackjackPlayers)
-        return Profits(value: blackjackProfits)
+        let winLoseResults = BlackjackGameJudge().winLoseResults(of: nonBlackjackScores, comparingWith: dealerScore)
+        
+        let winPlayers = winLoseResults.playerResults.filter { $0.winCount > $0.loseCount }.map { $0.player }
+        let losePlayers = winLoseResults.playerResults.filter { $0.winCount > $0.loseCount }.map { $0.player }
+        
+        let winPlayersProfit = plusProfits(of: winPlayers)
+        let losePlayersProfit = minusProfits(of: losePlayers)
+        
+        
+        let lhs = plusProfits(of: losePlayers).reduce(ZeroProfit(player: dealer) as Profit) { partialResult, profit in
+            partialResult.plus(profit)
+        }
+        
+        let rhs = plusProfits(of: winPlayers).reduce(ZeroProfit(player: dealer) as Profit) { partialResult, profit in
+            partialResult.plus(profit)
+        }
+        let sumblackjackProfits = plusProfits(of: blackjackPlayers).reduce(ZeroProfit(player: dealer) as Profit) { partialResult, profit in
+            partialResult.plus(profit)
+        }
+        
+        let dealerProfit = lhs.minus(rhs).minus(sumblackjackProfits)
+        
+        return Profits(value: [dealerProfit] + winPlayersProfit + losePlayersProfit + blackjackProfits)
+    }
+        
+        
+    private static func plusProfits(of players: [Player]) -> [Profit] {
+        players.map { PlusProfit(player: $0, money: $0.bettingMoney) }
     }
     
     private static func zeroProfits(of players: [Player]) -> [Profit] {
