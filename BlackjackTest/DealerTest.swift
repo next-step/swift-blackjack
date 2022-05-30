5d45c294c93704d600be716da2fd98c481d02cfc
woosub-lee <woosub.dev@gmail.com>
2022-05-30 11:23:48 +0900

    test: participant winstate when over 21

--- BlackjackTest/DealerTest.swift
+++ BlackjackTest/DealerTest.swift
@@ -62,4 +62,57 @@ class DealerTest: XCTestCase {
         
         XCTAssertEqual(participant.isWin(compareWith: dealer), .defeat)
     }
+    
+    func test_딜러와_참가자가_모두_21을_넘지않고_참가자가_더_클때_참가자는_승리() {
+        let participant = Participant(name: "sut1")
+        
+        dealer.add(card: Card(shape: .spade, number: .jack))
+        dealer.add(card: Card(shape: .spade, number: .queen))
+        
+        participant.add(card: Card(shape: .spade, number: .ace))
+        participant.add(card: Card(shape: .spade, number: .king))
+        
+        XCTAssertEqual(participant.isWin(compareWith: dealer), .win)
+    }
+    
+    func test_참가자만_21을_넘었을때_참가자는_패배() {
+        let participant = Participant(name: "sut1")
+        
+        dealer.add(card: Card(shape: .spade, number: .six))
+        dealer.add(card: Card(shape: .heart, number: .six))
+        dealer.add(card: Card(shape: .spade, number: .five))
+        
+        participant.add(card: Card(shape: .clover, number: .six))
+        participant.add(card: Card(shape: .diamond, number: .six))
+        participant.add(card: Card(shape: .heart, number: .king))
+        
+        XCTAssertEqual(participant.isWin(compareWith: dealer), .defeat)
+    }
+    
+    func test_딜러만_21을_넘었을때_참가자는_승리() {
+        let participant = Participant(name: "sut1")
+        
+        dealer.add(card: Card(shape: .clover, number: .six))
+        dealer.add(card: Card(shape: .diamond, number: .six))
+        dealer.add(card: Card(shape: .heart, number: .king))
+        
+        participant.add(card: Card(shape: .spade, number: .jack))
+        participant.add(card: Card(shape: .spade, number: .queen))
+        
+        XCTAssertEqual(participant.isWin(compareWith: dealer), .win)
+    }
+    
+    func test_딜러와_참가자_모두_21을_넘었을때_참가자는_패배() {
+        let participant = Participant(name: "sut1")
+        
+        dealer.add(card: Card(shape: .clover, number: .six))
+        dealer.add(card: Card(shape: .diamond, number: .six))
+        dealer.add(card: Card(shape: .heart, number: .king))
+        
+        participant.add(card: Card(shape: .spade, number: .six))
+        participant.add(card: Card(shape: .heart, number: .six))
+        participant.add(card: Card(shape: .spade, number: .king))
+        
+        XCTAssertEqual(participant.isWin(compareWith: dealer), .defeat)
+    }
 }
